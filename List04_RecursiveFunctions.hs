2052fdd2fd589d0c5e10be4171e3411610408b44
Moisés Jorge <moisesjorge30@gmail.com>
2023-01-10 10:08:39 +0100

    Update List04_RecursiveFunctions.hs
    Adicionei a funcao Factorial

--- List04_RecursiveFunctions.hs
+++ List04_RecursiveFunctions.hs
@@ -7,3 +7,9 @@ restoInt a b
     | a == b = 0
     | otherwise = restoInt (a - b) b
 
+-- 2º: Função que calcula o factorial de um numero
+factorial :: Int -> Int
+factorial n
+    | n == 0 || n == 1 = 1
+    | n > 1 = n * factorial (n-1)
+    | otherwise = -1 -- Não existe factorial de um número negativo
