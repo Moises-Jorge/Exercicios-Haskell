357975ca190cdb384afb0628a6743eb1376cc90b
Moisés Jorge <moisesjorge30@gmail.com>
2023-01-12 00:22:35 +0100

    Update List05_ListsFunctions.hs
    Add a funcao "elevadoAo3"

--- List05_ListsFunctions.hs
+++ List05_ListsFunctions.hs
@@ -4,3 +4,8 @@
 tamLista :: [a] -> Int
 tamLista [] = 0
 tamLista (cab : cauda) = 1 + tamLista cauda
+
+-- 2º: Função que recebe uma lista e rotorna outra lista que contem os elementos da primeira lista elevados ao cubo(3)
+elevadoAo3 :: [Int] -> [Int]
+elevadoAo3 [] = []
+elevadoAo3 (cab : cauda) = cab^3 : elevadoAo3 cauda
