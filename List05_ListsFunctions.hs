6555102f0e3ebd35ca344253327f9541d19ddd63
Moisés Jorge <moisesjorge30@gmail.com>
2023-01-12 03:42:30 +0100

    Update List05_ListsFunctions.hs
    Add as funções:
        somaElementos
        existeChar
        maiorDaLista

--- List05_ListsFunctions.hs
+++ List05_ListsFunctions.hs
@@ -9,3 +9,22 @@ tamLista (cab : cauda) = 1 + tamLista cauda
 elevadoAo3 :: [Int] -> [Int]
 elevadoAo3 [] = []
 elevadoAo3 (cab : cauda) = cab^3 : elevadoAo3 cauda
+
+-- 3º: Função que calcula o somatório dos elementos de uma lista de inteiro
+somaElementos :: [Int] -> Int
+somaElementos [] = 0
+somaElementos (cab : cauda) = cab + somaElementos cauda
+
+-- 4º: Função que verifica se uma string possui o caractere informado como parametro
+existeChar :: [Char] -> Char -> Bool
+existeChar [] _ = False
+existeChar (cab : cauda) ch
+    | ch == cab = True
+    | otherwise = existeChar cauda ch
+
+-- 5º: Função que retorna o maior valor de uma lista de inteiros
+maiorDaLista :: [Int] -> Int
+maiorDaLista [] = -1 -- Pode ser qualquer valor desde que se refira a uma lista vazia, pois cada numero pode representar um numero da lista
+maiorDaLista (cab : cauda)
+    | cab >= maiorDaLista cauda = cab
+    | otherwise = maiorDaLista cauda
